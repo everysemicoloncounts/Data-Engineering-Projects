81a2166f084b514db0f7bfc10c9be877068842eb
thehumblecodergp <gouravpratap.202cd006@nitk.edu.in>
2023-05-10 19:39:52 +0530

    queries added

--- Uber Data Analysis Project/Mage Files/load.py
+++ Uber Data Analysis Project/Mage Files/load.py
@@ -20,7 +20,7 @@ def export_data_to_big_query(data, **kwargs) -> None:
     config_profile = 'default'
 
     for key, value in data.items():
-        table_id = 'data-with-darshil.uber_data_engineering_yt.{}'.format(key)
+        table_id = 'data-with-gourav.uber_data_engineering_yt.{}'.format(key)
         BigQuery.with_config(ConfigFileLoader(config_path, config_profile)).export(
             DataFrame(value),
             table_id,
